d2d08ec2cd9c322dcfaeb6c74c3a833412ef498b
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-06-22 21:49:26 +0530

    Create OddNumbersAndString.cpp

---
+++ OddNumbersAndString.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+	
+  int num ;
+  printf("Enter the number: ");
+  scanf("%d", &num);
+  char str[50] = "";
+  int sum = 0;
+  
+  for (int i = 1; i < num*2; i = i+2) {
+  	char tempstr[10];
+  	snprintf(tempstr, sizeof(i),"%d ", i);
+  	strcat(str,tempstr);
+  	sum += i;
+  }
+  
+  printf("\nThe odd numbers are: %s", str);
+  printf("\nThe Sum: %d", sum);
+	
+}
+
