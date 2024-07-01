50bcc2e36f89565b9fdad3ec444e59967a7fb3f3
anusiyagwebtech <141471962+anusiyagwebtech@users.noreply.github.com>
2024-07-01 21:38:43 +0530

    Update Series Sum.cpp

--- Series Sum.cpp
+++ Series Sum.cpp
@@ -32,3 +32,26 @@ printf("\n\n%d", sum);
 
 	return 0;
 }
+
+#include <stdio.h>
+#include <Math.h>
+
+int main () {
+// Write a C program that displays the n terms of square natural numbers and their sum.
+//1 4 9 16 ... n Terms
+//Test Data :
+//Input the number of terms : 5
+//Expected Output :
+//The square natural upto 5 terms are :1 4 9 16 25
+//The Sum of Square Natural Number upto 5 terms = 55
+
+    int terms;
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+    int sum = 1;
+    for(int i = 2; i <= terms; i++) {
+    	sum += i*i;
+	}
+    printf("%d", sum);
+	return 0;
+}
